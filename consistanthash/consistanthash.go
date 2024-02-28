0d037bd70227b621534bba730f83446ea1753d7c
xban <x_ban@qq.com>
2024-02-28 18:32:03 +0800

    day4: consistant hash

---
+++ consistanthash/consistanthash.go
@@ -0,0 +1,59 @@
+package consistenthash
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+// Hash maps bytes to uint32
+type Hash func(data []byte) uint32
+
+// Map constains all hashed keys
+type Map struct {
+	hash     Hash
+	replicas int
+	keys     []int          // Sorted
+	hashMap  map[int]string // 虚拟节点 -> 真实节点
+}
+
+// New creates a Map instance
+func New(replicas int, fn Hash) *Map {
+	m := &Map{
+		replicas: replicas,
+		hash:     fn,
+		hashMap:  make(map[int]string),
+	}
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+	return m
+}
+
+// Add adds some keys to the hash.
+// 输入是实际查找 key，然后吧 key hash 并添加成虚拟节点
+func (m *Map) Add(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < m.replicas; i++ { // 这里来确实就是添加了几份虚拟节点，然后保存映射关系
+			hash := int(m.hash([]byte(strconv.Itoa(i) + key))) // 这种直接字符加来生成虚拟节点的方式，是不是有点不太好
+			m.keys = append(m.keys, hash)
+			m.hashMap[hash] = key
+		}
+	}
+	sort.Ints(m.keys)
+}
+
+// Get gets the closest item in the hash to the provided key.
+func (m *Map) Get(key string) string {
+	if len(m.keys) == 0 {
+		return ""
+	}
+
+	hash := int(m.hash([]byte(key)))
+	// Binary search for appropriate replica.
+	idx := sort.Search(len(m.keys), func(i int) bool {
+		return m.keys[i] >= hash
+	})
+
+	return m.hashMap[m.keys[idx%len(m.keys)]]
+}
